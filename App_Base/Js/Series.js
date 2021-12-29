f162618ca419f406470517a071df95de6f9c08d9
shahab.b2013@gmail.com <shahab.b2013@gmail.com>
2021-12-28 23:58:46 -0800

    Series_Edited

--- App_Base/Js/Series.js
+++ App_Base/Js/Series.js
@@ -11,7 +11,7 @@ function Series() {
   $("#seriesModal").css("width", "60%");
 
   let divGrid =
-    '<div class="" style="height:300px;border-bottom:1px solid #ccc;margin-bottom:10px"><button class="Add_series btn btn-success" style="margin-bottom:10px;" onclick="Add_SeriesList()">افزودن<i class="glyphicon glyphicon-plus-sign" style="margin-right:5px;"></i></button><table  class="table  table-bordered">' +
+    '<div class="" style="height:300px !important;border-bottom:1px solid #ccc;margin-bottom:10px;display:contents;"><button id="btnAddTolist" class="btn btn-success" style="margin-bottom:15px;padding:5px;width:70px;height:34px;" onclick="Form_Add_Series()">افزودن<i class="glyphicon glyphicon-plus-sign" style="margin:5px;"></i></button><table  class="table  table-bordered">' +
     "<thead>" +
     "<tr>" +
     '<th scope="col">ردیف</th>' +
@@ -31,9 +31,8 @@ function Series() {
     '<th scope="col">' +
     "عملیات" +
     "</th>" +
-    
     "</tr>" +
-    '</thead><tbody id="series_tbody" style="text-align: center;"></tbody></table></div>';
+    '</thead><tbody id="series_tbody" ></tbody></table></div>';
   $("#seriesModal").append(divGrid);
 
   //onload list
@@ -61,7 +60,7 @@ function Series() {
       '" class="btn btn-danger glyphicon glyphicon-trash span" onclick="Series_Deleted(id)"><i>حذف</i></span>' +
       '<span id="Series_Edit_' +
       item.ID +
-      '" class="btn btn-info glyphicon glyphicon-edit span onclick="Series_Edited(id)"><i>ویرایش</i></span>' +
+      '" class="btn btn-info glyphicon glyphicon-edit span" onclick="Series_Edited(id)"><i>ویرایش</i></span>' +
       "</td>" +
       "</tr>";
 
@@ -79,7 +78,7 @@ function Series() {
   Exit.onclick = () => $("#myModalSeries").remove();
 }
 
-function Add_SeriesList() {
+function Form_Add_Series(Text, id) {
   let div =
     '<div id="Add_Series" class="modal" style="padding-top: 190px;">' +
     '<div id="Add_seriesModal" class="modal-content">' +
@@ -92,70 +91,78 @@ function Add_SeriesList() {
   $("#Add_seriesModal").css("width", "20%");
 
   let form =
-    '<div class="" style="direction:ltr;margin-bottom:10px;border-bottom:1px solid #CCC;padding-bottom:10px;">' +
-    '<label class="lblSeries">Label:<input type="text" id="text1" class="TextboxSeries"></label><br>' +
-    '<label class="lblSeries">Name:<input type="text" id="text2" class="TextboxSeries" ></label><br>' +
-    '<label class="lblSeries">DataExpresstion:<input type="text" id="text3" class="TextboxSeries" ></label><br>' +
-    '<label class="lblSeries">Color:<input type="color" id="text4"></label><br>' +
+    '<div class="" style="direction:rtl;margin-bottom:10px;border-bottom:1px solid #CCC;padding-bottom:10px;">' +
+    '<label class="lblSeries">عنوان:<input type="text" id="text1" class="TextboxSeries"></label><br>' +
+    '<label class="lblSeries">نام:<input type="text" id="text2" class="TextboxSeries" ></label><br>' +
+    '<label class="lblSeries">فیلد داده ای:<input type="text" id="text3" class="TextboxSeries" ></label><br>' +
+    '<label class="lblSeries">رنگ:<input type="color" id="text4"></label><br>' +
     "</div>";
   $("#Add_seriesModal").append(form);
 
   //btnsubmit
   let Submit = btnSubmit("#Add_seriesModal");
+  Text == "edit" ? (Submit.innerHTML = "ویرایش") : (Submit.innerHTML = "ذخیره");
   Submit.onclick = () => {
-    //set ID
-    let _ID = 0;
-    let lastRow;
-    let rowCount = SERIES.length;
-    if (rowCount > 0) {
-      lastRow = +SERIES[rowCount - 1].ID;
-      _ID = lastRow + 1;
+    if (Text == "edit") {
+      //put SERIES
+      let tr = $("#" + id)
+        .parent()
+        .parent();
+      let rowID = tr.children().eq(0).html();
+      let Element = SERIES.find((Element) => Element.ID == rowID);
+      Element.Label = $("#text1").val();
+      Element.Name = $("#text2").val();
+      Element.DataExpresstion = $("#text3").val();
+      Element.Color = $("#text4").val();
+
+      //put table
+      tr.children().eq(1).html($("#text1").val());
+      tr.children().eq(2).html($("#text2").val());
+      tr.children().eq(3).html($("#text3").val());
+      tr.children().eq(4).html($("#text4").val());
+
+      //close
+      $("#Add_Series").remove();
     } else {
-      _ID = 1;
-    }
+      //set ID
+      let _ID = SERIES.length > 0 ? +SERIES[SERIES.length - 1].ID + 1 : 1;
 
-    let tbody =
-      "<tr>" +
-      '<td scope="row">' +
-      _ID +
-      "</td>" +
-      '<td scope="row">' +
-      $("#text1").val() +
-      "</td>" +
-      '<td scope="row">' +
-      $("#text2").val() +
-      "</td>" +
-      '<td scope="row">' +
-      $("#text3").val() +
-      "</td>" +
-      '<td scope="row">' +
-      $("#text4").val() +
-      "</td>" +
-      '<td scope="row">' +
-      '<span id="Series_Delete_' +
-      _ID +
-      '" class="btn btn-danger glyphicon glyphicon-trash span" onclick="Series_Deleted(id)"><i>حذف</i></span>' +
-      '<span id="Series_Edit_' +
-      _ID +
-      '" class="btn btn-info glyphicon glyphicon-edit span onclick="Series_Edited(id)"><i>ویرایش</i></span>' +
-      "</td>" +
-      "</tr>";
+      //Add to tbody in table
+      let tbody =
+        "<tr>" +
+        '<td scope="row">' +
+        _ID +
+        "</td>" +
+        '<td scope="row">' +
+        $("#text1").val() +
+        "</td>" +
+        '<td scope="row">' +
+        $("#text2").val() +
+        "</td>" +
+        '<td scope="row">' +
+        $("#text3").val() +
+        "</td>" +
+        '<td scope="row">' +
+        $("#text4").val() +
+        "</td>" +
+        '<td scope="row">' +
+        '<span id="Series_Delete_' +
+        _ID +
+        '" class="btn btn-danger glyphicon glyphicon-trash span" onclick="Series_Deleted(id)"><i style="top:5px">حذف</i></span>' +
+        '<span id="Series_Edit_' +
+        _ID +
+        '" class="btn btn-info glyphicon glyphicon-edit span" onclick="Series_Edited(id)"><i>ویرایش</i></span>' +
+        "</td>" +
+        "</tr>";
 
-    $("#series_tbody").append(tbody);
+      $("#series_tbody").append(tbody);
 
-    //cleae SERIES
-    while (SERIES.length) {
-      SERIES.pop();
-    }
-
-    //get list set series array
-    $("#series_tbody tr").each(function () {
-      const ID = $(this).find("td").eq(0).html();
-      const Label = $(this).find("td").eq(1).html();
-      const Name = $(this).find("td").eq(2).html();
-      const DataExpresstion = $(this).find("td").eq(3).html();
-      const Color = $(this).find("td").eq(4).html();
-
+      //set SERIES
+      const ID = _ID;
+      const Label = $("#text1").val();
+      const Name = $("#text2").val();
+      const DataExpresstion = $("#text3").val();
+      const Color = $("#text4").val();
       SERIES.push({
         ID,
         Label,
@@ -163,22 +170,21 @@ function Add_SeriesList() {
         DataExpresstion,
         Color,
       });
-    });
 
-    $("#Add_Series").remove();
+      //close
+      $("#Add_Series").remove();
+    }
   };
-
   //btn exit
   let Exit = btnExit("#Add_seriesModal");
-  Exit.onclick = function () {
-    $("#Add_Series").remove();
-  };
+  Exit.onclick = () => $("#Add_Series").remove();
 }
 
 function Series_Deleted(id) {
   let tr = $("#" + id)
     .parent()
     .parent();
+
   // remove from list
   tr.remove();
 
@@ -189,4 +195,18 @@ function Series_Deleted(id) {
     1
   );
 }
-function Series_Edited(e) {}
+function Series_Edited(id) {
+  let tr = $("#" + id)
+    .parent()
+    .parent();
+  let rowID = tr.children().eq(0).html();
+
+  Form_Add_Series("edit", id);
+
+  //set Form_Add_Series Items
+  let items = SERIES.find((Element) => Element.ID == rowID);
+  $("#text1").val(items.Label);
+  $("#text2").val(items.Name);
+  $("#text3").val(items.DataExpresstion);
+  $("#text4").val(items.Color);
+}
